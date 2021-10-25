1d767c1cf58d754b3ba662577e831fbbb56ef5ea
thibrub92 <thibruban@gmail.com>
2021-10-25 19:58:05 +0200

    add test

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -16,9 +16,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.core.IsNull.notNullValue;
 
@@ -67,5 +72,45 @@ public class NeighboursListTest {
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
         // Then : the number of element is 11
         onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
+    }
+    @Test
+    public void myNeighboursList_AddAction_shouldAddItem(){
+        onView(withId(R.id.add_neighbour)).perform(click());
+
+        onView(withId(R.id.name)).perform(typeText("name"));
+        onView(withId(R.id.name)).perform(pressImeActionButton());
+
+        onView(withId(R.id.phoneNumber)).perform(typeText("0707070707"));
+        onView(withId(R.id.phoneNumber)).perform(pressImeActionButton());
+
+        onView(withId(R.id.address)).perform(typeText("1 rue de rivoli, 75001 Paris"));
+        onView(withId(R.id.address)).perform(pressImeActionButton());
+
+        onView(withId(R.id.aboutMe)).perform(typeText("about me"));
+
+        pressBack();
+
+        onView(withId(R.id.create)).perform(click());
+
+        onView(withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT +1));
+
+        ITEMS_COUNT = ITEMS_COUNT +1;
+
+    }
+    @Test
+    public void myNeighboursList_check_favoriteList_removeItem(){
+        onView(withId(R.id.item_list_name)).perform(click());
+
+        onView(withId(R.id.favoriteButton)).perform(click());
+
+        pressBack();
+
+        onView(withId(R.id.tabItem2)).check(withItemCount(ITEMS_COUNT +1));
+
+        onView(withId(R.id.item_list_delete_button)).perform(click());
+
+        onView(withId(R.id.list_favorite_neighbours)).check(withItemCount(ITEMS_COUNT -1));
+
+        ITEMS_COUNT = ITEMS_COUNT -1;
     }
 }
